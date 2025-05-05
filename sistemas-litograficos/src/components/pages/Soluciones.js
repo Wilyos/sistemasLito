88d319999a711313bf7010c4ce18684d35aeff8b
wilyos <wilyos99@gmail.com>
2025-05-05 15:00:44 -0500

    Portafolio-solucioenes base final

--- sistemas-litograficos/src/components/pages/Soluciones.js
+++ sistemas-litograficos/src/components/pages/Soluciones.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import '../../App.css';
 
 
@@ -7,49 +7,99 @@ import '../../App.css';
 
 function Soluciones(){
 
+    const [selectedCategory, setSelectedCategory] = useState("todos");
+
     const products = [
-        { id: 1, name: 'Editorial', image: require('../../assets/editorialPortafolio.jpg') },
-        { id: 2, name: 'Publicomerciales', image: require('../../assets/afichePortafolio.png') },
-        { id: 3, name: 'Packaging', image: require('../../assets/packagingPortafolio.jpg') },
-        { id: 4, name: 'Corporativa', image: require('../../assets/corporativasPortafolio.jpg') },
-        { id: 5, name: 'Publicidad Exterior', image: require('../../assets/exterioresPortafolio.png') },
-        { id: 6, name: 'Material P.O.P', image: require('../../assets/popPortafolio.jpg') }
+        { id: 1, name: 'Editorial', image: require('../../assets/editorialPortafolio.jpg'), categoria: 'Editorial' },
+        { id: 2, name: 'Publicomerciales', image: require('../../assets/afichePortafolio.png'), categoria: 'Publicidad' },
+        { id: 3, name: 'Packaging', image: require('../../assets/packagingPortafolio.jpg'), categoria: 'Empaque' },
+        { id: 4, name: 'Corporativa', image: require('../../assets/corporativasPortafolio.jpg'), categoria: 'Corporativo' },
+        { id: 5, name: 'Publicidad Exterior', image: require('../../assets/exterioresPortafolio.png'), categoria: 'PublicidadEx' },
+        { id: 6, name: 'Material P.O.P', image: require('../../assets/popPortafolio.jpg'), categoria: 'MaterialPop' }
     ];
 
-    return(
+    const filteredProducts = selectedCategory === "todos"
+    ? products
+    : products.filter(product => product.categoria === selectedCategory);
+
+    return (
         <>
             <div className="wrapper-solucion">
                 <aside>
                     <navbar>
-                        <ul className="menu">
+                        <ul className="menu-aside">
                             <li>
-                                <button className="boton-categoria"><i class="fa-solid fa-hand-point-right"></i> Todos los productos</button>
+                                <button
+                                    id="todos"
+                                    className={`boton-categoria ${selectedCategory === "todos" ? "active" : ""}`}
+                                    onClick={() => setSelectedCategory("todos")}
+                                >
+                                    <i className="fa-solid fa-hand-point-right"></i> Todos los productos
+                                </button>
                             </li>
                             <li>
-                                <button className="boton-categoria"><i class="fa-regular fa-hand-point-right"></i> Editoriales</button>
+                                <button
+                                    id="Editorial"
+                                    className={`boton-categoria ${selectedCategory === "Editorial" ? "active" : ""}`}
+                                    onClick={() => setSelectedCategory("Editorial")}
+                                >
+                                    <i className="fa-regular fa-hand-point-right"></i> Editoriales
+                                </button>
                             </li>
                             <li>
-                                <button className="boton-categoria"><i class="fa-regular fa-hand-point-right"></i> Empaques</button>
+                                <button
+                                    id="Publicidad"
+                                    className={`boton-categoria ${selectedCategory === "Publicidad" ? "active" : ""}`}
+                                    onClick={() => setSelectedCategory("Publicidad")}
+                                >
+                                    <i className="fa-regular fa-hand-point-right"></i> Publicidad
+                                </button>
                             </li>
                             <li>
-                                <button className="boton-categoria"><i class="fa-regular fa-hand-point-right"></i> Corportativo</button>
+                                <button
+                                    id="Empaque"
+                                    className={`boton-categoria ${selectedCategory === "Empaque" ? "active" : ""}`}
+                                    onClick={() => setSelectedCategory("Empaque")}
+                                >
+                                    <i className="fa-regular fa-hand-point-right"></i> Empaques
+                                </button>
                             </li>
                             <li>
-                                <button className="boton-categoria"><i class="fa-regular fa-hand-point-right"></i> Publicidad</button>
+                                <button
+                                    id="Corporativo"
+                                    className={`boton-categoria ${selectedCategory === "Corporativo" ? "active" : ""}`}
+                                    onClick={() => setSelectedCategory("Corporativo")}
+                                >
+                                    <i className="fa-regular fa-hand-point-right"></i> Corporativo
+                                </button>
                             </li>
                             <li>
-                                <button className="boton-categoria"><i class="fa-regular fa-hand-point-right"></i> Publicidad Exterior</button>
+                                <button
+                                    id="PublicidadEx"
+                                    className={`boton-categoria ${selectedCategory === "PublicidadEx" ? "active" : ""}`}
+                                    onClick={() => setSelectedCategory("PublicidadEx")}
+                                >
+                                    <i className="fa-regular fa-hand-point-right"></i> Publicidad Exterior
+                                </button>
+                            </li>
+                            <li>
+                                <button
+                                    id="MaterialPop"
+                                    className={`boton-categoria ${selectedCategory === "MaterialPop" ? "active" : ""}`}
+                                    onClick={() => setSelectedCategory("MaterialPop")}
+                                >
+                                    <i className="fa-regular fa-hand-point-right"></i> Material P.O.P
+                                </button>
                             </li>
                         </ul>
                     </navbar>
                 </aside>
                 <main className="main-soluciones">
                     <h2 className="titulo-soluciones">
-                        Todos los productos
+                        {selectedCategory === "todos" ? "Todos los productos" : selectedCategory}
                     </h2>
                     <div className="contenedor-soluciones">
-                        <div className="productos">
-                        {products.map(product => (
+                        {filteredProducts.map(product => (
                             <div key={product.id} className="productos">
                                 <img className="producto-imagen-soluciones" src={product.image} alt={product.name} />
                                 <div className="producto-detalles">
@@ -57,12 +107,11 @@ function Soluciones(){
                                 </div>
                             </div>
                         ))}
-                        </div>
                     </div>
                 </main>
             </div>
         </>
-    )
+    );
 
 }
 
