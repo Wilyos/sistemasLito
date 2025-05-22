17569d0bda1033c3cf969836fed84ce2f535b7e1
wilyos <wilyos99@gmail.com>
2025-05-22 08:42:55 -0500

    google ads +

--- sistemas-litograficos/src/components/pages/Confirmacion.js
+++ sistemas-litograficos/src/components/pages/Confirmacion.js
@@ -1,28 +1,23 @@
-import React, { useEffect } from "react"; 
+import React, { useEffect } from "react";
 import "../../App.css";
 
-const Confirmacion =()=> {
+const Confirmacion = () => {
   useEffect(() => {
-    const script = document.createElement("script");
-    script.innerHTML = `
-      gtag('event', 'conversion', {
-        'send_to': 'AW-775722605/h7kaCN3js4ADEO2s8vEC',
-        'transaction_id': ''
+    if (window.gtag) {
+      window.gtag("event", "conversion", {
+        send_to: "AW-775722605/h7kaCN3js4ADE02s8vEC",
       });
-    `;
-    document.body.appendChild(script);
-
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []); 
+    } else {
+      console.warn("gtag no está definido aún.");
+    }
+  }, []);
 
   return (
     <div className="confirmacion-container">
-      <h1>¡Mensaje enviado con éxito!</h1>
+      <h1>Mensaje enviado con éxito!</h1>
       <p>Gracias por contactarnos. Nos comunicaremos contigo lo más pronto posible.</p>
     </div>
   );
-}
+};
 
 export default Confirmacion;
