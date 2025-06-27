b403157204abd5d479ef78858891571cf08ef819
Wil Bohorquez <wilyos99@gmail.com>
2025-06-27 16:26:23 -0500

    cambio de imagenes en carousell para vista mas estetica

--- sistemas-litograficos/src/components/Clientes.js
+++ sistemas-litograficos/src/components/Clientes.js
@@ -11,26 +11,26 @@ const Clientes= ()=> {
     const { t } = useTranslation();
 
     const clientes = [
-        { id: 1, name: 'Autolarte', image: require('../assets/AUTOLARTE.png') },
-        { id: 2, name: 'Alcaldia-Bello', image: require('../assets/bello.png') },
-        { id: 3, name: 'cats', image: require('../assets/Cats_Boston2.png') },
-        { id: 4, name: 'Fedean', image: require('../assets/fedean.png') },
-        { id: 5, name: 'Pizza_Doble_Pizza', image: require('../assets/Pizza_Doble_Pizza.png') },
-        { id: 6, name: 'Tiendas_D1', image: require('../assets/D1.png') },
-        { id: 7, name: 'Universidad_de_Antioquia', image: require('../assets/UDEA.png') },
-        { id: 8, name: 'CA Consultores', image: require('../assets/CA-CONSULTORES.png') },
-        { id: 9, name: 'Comfenalco', image: require('../assets/COMFENALCO.png') },
-        { id: 10, name: 'drogeria-todo-drogas', image: require('../assets/DROGUERIA-TODO-DROGAS.png') },
-        { id: 11, name: 'INSTITUTO-FERRINI', image: require('../assets/INSTITU-FERRINI.png') },
-        { id: 12, name: 'LINEA-E-METRO', image: require('../assets/LINEA-E-METRO.png') },
-        { id: 13, name: 'LUMA', image: require('../assets/LUMA.png') },
-        { id: 14, name: 'OSANA', image: require('../assets/OSANA.png') },
-        { id: 15, name: 'PEGAKLEIN', image: require('../assets/PEGAKLEIN.png') },
-        { id: 16, name: 'PINVIACRIL', image: require('../assets/PINVIACRIL.png') },
-        { id: 17, name: 'PIZZA-PREMIUM', image: require('../assets/PIZZA-PREMIUM.png') },
-        { id: 18, name: 'rionegro', image: require('../assets/rionegro.png') },
-        { id: 19, name: 'TECNIDECOR', image: require('../assets/TECNIDECOR.png') },
-        { id: 20, name: 'TICKETXPRESS', image: require('../assets/TICKETXPRESS.png') }
+        { id: 1, name: 'Autolarte', image: require('../assets/AUTOLARTEv2.png') },
+        { id: 2, name: 'Alcaldia-Bello', image: require('../assets/ALCALDIA DE BELLOv2.png') },
+        { id: 3, name: 'cats', image: require('../assets/CATSv2.png') },
+        { id: 4, name: 'Fedean', image: require('../assets/FEDEANv2.png') },
+        { id: 5, name: 'Pizza_Doble_Pizza', image: require('../assets/PIZZA DOBLE PIZZAv2.png') },
+        { id: 6, name: 'Tiendas_D1', image: require('../assets/D1v2.png') },
+        { id: 7, name: 'Universidad_de_Antioquia', image: require('../assets/UDEAv2.png') },
+        { id: 8, name: 'CA Consultores', image: require('../assets/CACONSULTORESv2.png') },
+        { id: 9, name: 'Comfenalco', image: require('../assets/COMFENALCOv2.png') },
+        { id: 10, name: 'drogeria-todo-drogas', image: require('../assets/DROGUERIA TODO DROGASv2.png') },
+        { id: 11, name: 'INSTITUTO-FERRINI', image: require('../assets/INSTITU FERRINIv2.png') },
+        { id: 12, name: 'LINEA-E-METRO', image: require('../assets/LINEA E METROv2.png') },
+        { id: 13, name: 'LUMA', image: require('../assets/LUMAv2.png') },
+        { id: 14, name: 'OSANA', image: require('../assets/OSANAv2.png') },
+        { id: 15, name: 'PEGAKLEIN', image: require('../assets/PEGAKLEINv2.png') },
+        { id: 16, name: 'PINVIACRIL', image: require('../assets/PINVIACRILv2.png') },
+        { id: 17, name: 'PIZZA-PREMIUM', image: require('../assets/PIZZA PREMIUMv2.png') },
+        { id: 18, name: 'rionegro', image: require('../assets/ALCALDIA DE RIONEGROv2.png') },
+        { id: 19, name: 'TECNIDECOR', image: require('../assets/TECNIDECORv2.png') },
+        { id: 20, name: 'TICKETXPRESS', image: require('../assets/TICKETXPRESSv2.png') }
     ];
 
     const settings = {
