ed19f268c976d54f287bd198da6ee022a943d342
Wil Bohorquez <wilyos99@gmail.com>
2025-09-11 15:16:08 -0500

    fix meta pixel

--- sistemas-litograficos/src/components/pages/Confirmacion.js
+++ sistemas-litograficos/src/components/pages/Confirmacion.js
@@ -3,14 +3,12 @@ import { useTranslation } from 'react-i18next';
 import "../../App.css";
 
 const Confirmacion =()=> {
-  const {t} = useTranslation
+  const { t } = useTranslation();
   useEffect(() => {
-    if (window.gtag) {
-      window.gtag("event", "conversion", {
-        send_to: "AW-775722605/h7kaCN3js4ADE02s8vEC",
-      });
+    if (window.fbq) {
+      window.fbq('track', 'Lead');
     } else {
-      console.warn("gtag no está definido aún.");
+      console.warn('fbq no está definido aún.');
     }
   }, []);
 
